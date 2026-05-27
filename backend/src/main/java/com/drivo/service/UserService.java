cfc24e13885f124834b0a0ee6ec1b3e31cc73988
Anshad Zaman <anshadzaman10@gmail.com>
2026-05-27 11:50:15 +0530

    create user service

--- backend/src/main/java/com/drivo/service/UserService.java
+++ backend/src/main/java/com/drivo/service/UserService.java
@@ -1,4 +1,28 @@
 package com.drivo.service;
 
+import com.drivo.entity.User;
+import com.drivo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+
+        return userRepository.save(user);
+
+    }
+
+    public List<User> getAllUsers(){
+
+        return userRepository.findAll();
+
+    }
+
 }
