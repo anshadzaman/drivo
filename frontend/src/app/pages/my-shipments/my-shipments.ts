936d048d8134cfe498c3eae136940f240fb68b8b
Anshad Zaman <anshadzaman10@gmail.com>
2026-06-07 11:14:36 +0530

    added driver shipment status filters

--- frontend/src/app/pages/my-shipments/my-shipments.ts
+++ frontend/src/app/pages/my-shipments/my-shipments.ts
@@ -1,69 +1,80 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { ShipmentService }
-from '../../services/shipment.service';
+  from '../../services/shipment.service';
 import { ChangeDetectorRef } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Shipment } from '../../models/shipment';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-my-shipments',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './my-shipments.html',
   styleUrl: './my-shipments.css'
 })
 export class MyShipments
-implements OnInit {
+  implements OnInit {
 
   shipments: any[] = [];
-
- constructor(
-  private shipmentService: ShipmentService,
-  private cdr: ChangeDetectorRef
-) {} 
+  filter = 'ALL';
+  constructor(
+    private shipmentService: ShipmentService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
 
-  console.log("MyShipments loaded");
+    console.log("MyShipments loaded");
 
- this.shipmentService
-    .getMyShipments()
-    .subscribe((data: any) => {
+    this.shipmentService
+      .getMyShipments()
+      .subscribe((data: any) => {
 
-      console.log("API DATA:", data);
+        console.log("API DATA:", data);
 
-      this.shipments = data;
+        this.shipments = data;
 
-      this.cdr.detectChanges();
+        this.cdr.detectChanges();
 
-    });
+      });
 
-}
-markDelivered(
-  id: number
-) {
+  }
+  markDelivered(
+    id: number
+  ) {
 
-  this.shipmentService
+    this.shipmentService
       .markMyShipmentDelivered(
         id
       )
       .subscribe(() => {
-
+        this.cdr.detectChanges();
         this.shipmentService
-            .getMyShipments()
-            .subscribe(
-              (data: any) => {
+          .getMyShipments()
+          .subscribe(
+            (data: any) => {
 
-                this.shipments =
-                  data;
+              this.shipments =
+                data;
 
-                this.cdr
-                    .detectChanges();
+              this.cdr
+                .detectChanges();
 
-              }
-            );
+            }
+          );
 
       });
 
-} 
+  }
+  get filteredShipments() {
 
+    if (this.filter === 'ALL') {
+      return this.shipments;
+    }
+
+    return this.shipments.filter(
+      s => s.status === this.filter
+    );
+
+  }
 }
