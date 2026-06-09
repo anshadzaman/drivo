e24042705da539be3c45c17d57282b09038f0606
Anshad Zaman <anshadzaman10@gmail.com>
2026-06-09 11:12:42 +0530

    implemented user management page

--- frontend/src/app/pages/users/users.ts
+++ frontend/src/app/pages/users/users.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
-export class Users {}
+export class Users implements OnInit {
+
+  users: any[] = [];
+
+  filteredUsers: any[] = [];
+
+  searchText = '';
+
+  constructor(
+    private userService: UserService,
+    private cdr : ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+
+    this.userService
+        .getUsers()
+        .subscribe((data: any) => {
+
+          this.users = data;
+
+          this.filteredUsers = data;
+this.cdr.detectChanges();
+        });
+
+  }
+
+  searchUsers() {
+
+    this.filteredUsers =
+      this.users.filter(user =>
+
+        user.name
+            .toLowerCase()
+            .includes(
+              this.searchText.toLowerCase()
+            )
+
+        ||
+
+        user.email
+            .toLowerCase()
+            .includes(
+              this.searchText.toLowerCase()
+            )
+
+      );
+
+  }
+
+}
