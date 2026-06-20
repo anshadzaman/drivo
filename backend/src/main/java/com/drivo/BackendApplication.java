b7ff004d1469d7d3dcc83bcf8e0ca7b7c70970e0
Anshad Zaman <anshadzaman10@gmail.com>
2026-06-20 11:13:59 +0530

    Set default timezone to Asia/Kolkata

--- backend/src/main/java/com/drivo/BackendApplication.java
+++ backend/src/main/java/com/drivo/BackendApplication.java
@@ -3,11 +3,20 @@ package com.drivo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(
+				TimeZone.getTimeZone(
+						"Asia/Kolkata"
+				)
+		);
 		SpringApplication.run(BackendApplication.class, args);
+
+
 	}
 
 }
